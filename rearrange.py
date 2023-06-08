d06fd077ded11f254efe61661402c3a0e8c29c7f
sabdulmajid <ayman.hasib@outlook.com>
2023-06-09 02:49:15 +0300

    removed the .pdf with os.path.splittext()

--- rearrange.py
+++ rearrange.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-pdf_directory = r'path/to/pdf/files'
-output_file = 'lectures.txt'
+pdf_directory = r'path\to\pdf\files'
+output_file = 'lectures_arranged2.txt'
 
 # Get the list of PDF files in the directory
 pdf_files = [file for file in os.listdir(pdf_directory) if file.lower().endswith('.pdf')]
@@ -18,9 +18,9 @@ for pdf_file in pdf_files:
 # Sort the list of files based on lecture numbers
 sorted_files = sorted(lecture_numbers, key=lambda x: x[1])
 
-# Append sorted PDF file names to the output file
+# Append sorted PDF file names (without extension) to the output file
 with open(output_file, 'w') as file:
     for pdf_file, _ in sorted_files:
-        file.write(pdf_file + '\n')
+        file.write(os.path.splitext(pdf_file)[0] + '\n')
 
 print("File names sorted and appended successfully.")
