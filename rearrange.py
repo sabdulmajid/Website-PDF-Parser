234baa474195d1432010db7f4785c6b25e299efd
sabdulmajid <ayman.hasib@outlook.com>
2023-06-09 02:47:41 +0300

    modified python script to arrange numerically

---
+++ rearrange.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+pdf_directory = r'path/to/pdf/files'
+output_file = 'lectures.txt'
+
+# Get the list of PDF files in the directory
+pdf_files = [file for file in os.listdir(pdf_directory) if file.lower().endswith('.pdf')]
+
+# Extract lecture numbers from the file names using regular expressions
+lecture_numbers = []
+for pdf_file in pdf_files:
+    match = re.search(r'Lecture (\d+)', pdf_file)
+    if match:
+        lecture_number = int(match.group(1))
+        lecture_numbers.append((pdf_file, lecture_number))
+
+# Sort the list of files based on lecture numbers
+sorted_files = sorted(lecture_numbers, key=lambda x: x[1])
+
+# Append sorted PDF file names to the output file
+with open(output_file, 'w') as file:
+    for pdf_file, _ in sorted_files:
+        file.write(pdf_file + '\n')
+
+print("File names sorted and appended successfully.")
